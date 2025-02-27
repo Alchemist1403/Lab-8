51f8da33d8081d1c5f36be3824e95aa1e754b162
Alchemist <Alchemist1403@yandex.ru>
2025-02-27 18:27:43 +0300

    New changes

--- task2.py
+++ task2.py
@@ -1,9 +1,14 @@
 import cv2
-
+import numpy as np
 
 def video_processing():
+    ''' Вариант 8
+        Слежка за меткой.
+        В кадре отображаются горизонтальная и веритикальная
+        прямые, которые пересекаются в центре метки.
+    '''
     cap = cv2.VideoCapture(0)
-    down_points = (640, 480)
+    down_points = (900, 750)
 
     while True:
         ret, frame = cap.read()
@@ -11,14 +16,14 @@ def video_processing():
             break
 
         frame = cv2.resize(frame, down_points, interpolation=cv2.INTER_LINEAR)
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
 
+        ret, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY_INV)
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         if len(contours) > 0:
-
             c = max(contours, key=cv2.contourArea)
             x, y, w, h = cv2.boundingRect(c)
 
@@ -27,6 +32,7 @@ def video_processing():
             # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         cv2.imshow('Detection', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
